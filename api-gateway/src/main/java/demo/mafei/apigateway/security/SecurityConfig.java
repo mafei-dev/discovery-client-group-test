baf614f5019a8bd2e9abd465b967f527405856f5
mafei <mafei.dev@gmail.com>
2022-08-05 18:59:20 +0530

    import fixed

--- api-gateway/src/main/java/demo/mafei/apigateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/demo/mafei/apigateway/security/SecurityConfig.java
@@ -4,12 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
